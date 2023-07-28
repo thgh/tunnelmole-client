d95084b88244107d5d53bff3bc4708d231f24760
Thomas Ghysels <info@thomasg.be>
2023-07-28 15:11:57 +0200

    Remove reference to missing file: config-instance.js

--- config.ts
+++ config.ts
@@ -1,17 +1,16 @@
-import instanceConfig from "./config-instance.js";
-import deepmerge from 'deepmerge';
+import deepmerge from 'deepmerge'
 
 const baseConfig = {
-    hostip: {
-        endpoint: "service.tunnelmole.com",
-        port: "80"
-    },
-    runtime: {
-        enableLogging: true
-    }    
+  hostip: {
+    httpEndpoint: 'https://service.tunnelmole.com',
+    endpoint: 'https://service.tunnelmole.com',
+    port: '80',
+  },
+  runtime: {
+    enableLogging: true,
+  },
 }
 
+const config = deepmerge(baseConfig, {})
 
-const config = deepmerge(baseConfig, instanceConfig);
-
-export default config;
+export default config
